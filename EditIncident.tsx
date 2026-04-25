2740866981194635401aca69bf51273c8269a4dd
cdossierx <Crystaldcraig@outlook.com>
2026-04-25 16:56:21 -0500

    Update import path for trpc in EditIncident.tsx

--- EditIncident.tsx
+++ EditIncident.tsx
@@ -1,4 +1,4 @@
-import { trpc } from "./providers/trpc";
+import { trpc } from "./trpc";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { Label } from "./components/ui/label";
