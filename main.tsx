7dbfab7ae5e474c86269ce89a640376dfdca0de1
cdossierx <Crystaldcraig@outlook.com>
2026-04-25 15:56:31 -0500

    Update import paths for BrowserRouter and components

--- main.tsx
+++ main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import { TRPCProvider } from "@/providers/trpc"
-import { Toaster } from "@/components/ui/sonner"
+import { TRPCProvider } from "../providers/trpc"
+import { Toaster } from "../components/ui/sonner"
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
