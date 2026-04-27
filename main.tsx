c3ff51be1187faf1565600c56fcab2bbdc1322ed
cdossierx <Crystaldcraig@outlook.com>
2026-04-26 23:27:58 -0500

    Refactor import path for Toaster component

--- main.tsx
+++ main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { TRPCProvider } from "../providers/trpc"
-import { Toaster } from "../components/ui/sonner"
+import { Toaster } from "../ui/sonner"
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
