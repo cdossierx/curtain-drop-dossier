290d9ac6c66f065f9a8ad8d0953b24845d66bae4
cdossierx <Crystaldcraig@outlook.com>
2026-04-26 21:49:03 -0500

    Refactor import path for AppLayout component

--- Tags.tsx
+++ Tags.tsx
@@ -1,5 +1,5 @@
 import { trpc } from "./trpc"
-import { AppLayout } from "./components/AppLayout";
+import { AppLayout } from "./AppLayout";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { Label } from "./components/ui/label";
