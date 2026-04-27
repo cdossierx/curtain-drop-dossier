e071bea10ce10ee87fddf731189f1238585fccb8
cdossierx <Crystaldcraig@outlook.com>
2026-04-27 00:08:24 -0500

    Fix import path for httpBatchLink

--- trpc.tsx
+++ trpc.tsx
@@ -1,5 +1,5 @@
 import { createTRPCReact } from ".trpc/react-query";
-import { httpBatchLink } from "@/trpc/client";
+import { httpBatchLink } from "@trpc/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import superjson from "superjson";
 import type { AppRouter } from "./api/router";
