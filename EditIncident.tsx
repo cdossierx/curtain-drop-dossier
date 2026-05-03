bf3f1a22fc69cc8dd15ad27c98d571425589f27a
cdossierx <Crystaldcraig@outlook.com>
2026-05-03 01:31:20 -0500

    Update EditIncident.tsx

--- EditIncident.tsx
+++ EditIncident.tsx
@@ -1,5 +1,5 @@
 import { trpc } from "./trpc";
-import { Button } from "../components/ui/button";
+import { Button } from "../ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
