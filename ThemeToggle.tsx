39752b787835e3dd27ea49b3e24a61516cb8d6cd
cdossierx <Crystaldcraig@outlook.com>
2026-05-04 01:00:32 -0500

    ./ui/dialog repair

--- ThemeToggle.tsx
+++ ThemeToggle.tsx
@@ -1,5 +1,5 @@
 import { Moon, Sun } from "lucide-react";
-import { Button } from "../ui/button";
+import { Button } from "./ui/button";
 import { useState, useEffect } from "react";
 
 export function ThemeToggle() {
