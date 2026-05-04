f9159164e6a070a3c7be0c33d5e3b2d38c47ad67
cdossierx <Crystaldcraig@outlook.com>
2026-05-04 12:09:50 -0500

    fix utils typo

--- sidebar.tsx
+++ sidebar.tsx
@@ -6,7 +6,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { PanelLeftIcon } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
-import { cn } from "./lib/utilss"
+import { cn } from "./lib/utils"
 import { Button } from "./button"
 import { Input } from "./input"
 import { Separator } from "./separator"
--- tooltip.tsx
+++ tooltip.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
-import { cn } from "./lib/utilss"
+import { cn } from "./lib/utils"
 
 function TooltipProvider({
   delayDuration = 0,
