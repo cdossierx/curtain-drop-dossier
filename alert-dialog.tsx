fa7d4b2a8d6b7d85a8702e8b7077fd1a7ae81075
cdossierx <Crystaldcraig@outlook.com>
2026-05-04 11:55:46 -0500

    fix lib utils aliases

--- alert-dialog.tsx
+++ alert-dialog.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
-import { cn } from "@/lib/utils"
+import { cn } from "./lib/utilss"
 import { buttonVariants } from "./button"
 
 function AlertDialog({
