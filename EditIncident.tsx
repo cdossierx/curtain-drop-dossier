0d172f2a8718496122f8f8ed751265547be12b14
cdossierx <Crystaldcraig@outlook.com>
2026-05-04 03:03:55 -0500

    Update EditIncident.tsx

--- EditIncident.tsx
+++ EditIncident.tsx
@@ -4,7 +4,7 @@ import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
-import { Slider } from "./ui/slider";
+import { Slider } from "./ui/sliders";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
 import { useState, useEffect } from "react";
