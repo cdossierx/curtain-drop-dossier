9ac480086d38a50b0a7413610cb99e93e4e2fe91
cdossierx <Crystaldcraig@outlook.com>
2026-05-04 22:10:47 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router'
-import Dashboard from './pages/Dashboard'
+import Dashboard from './Dashboard'
 import LogIncident from './pages/LogIncident'
 import Timeline from './pages/Timeline'
 import Intake from './pages/Intake'
