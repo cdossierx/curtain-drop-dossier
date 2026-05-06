a44122dd3bbea8191562c028dbe4e856b044d3b3
cdossierx <Crystaldcraig@outlook.com>
2026-05-06 14:53:27 -0500

    fix app router

--- providers/trpc.tsx
+++ providers/trpc.tsx
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
+import { createTRPCReact } from @trpc/react-query';
+import type { AppRouter } from '../src/server';
+
+export const trpc = createTRPCReact<AppRouter>();
+>>>>>>> b74a81b0d6792fe1519ec0a770eea463cc63d63b
+
 export const trpc = createTRPCReact<AppRouter>();
