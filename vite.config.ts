d006b12822ddc02e05fd71d2d5db75cd485b5ddd
cdossierx <Crystaldcraig@outlook.com>
2026-05-07 15:36:50 +0000

    fix railway vite host

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  
+  
+  
+  
   resolve: {
     alias: {
       "@": path.resolve(process.cwd()),
